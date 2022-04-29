74ef3df25ff7fafd9ee540a37a3d64fa6917701c
wy199710 <wy199710@163.com>
2022-04-29 00:24:51 -0400

    main_initial

--- src/main/java/com/example/db_final/DbFinalApplication.java
+++ src/main/java/com/example/db_final/DbFinalApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DbFinalApplication {
 
     public static void main(String[] args) {
+        System.out.println("hello world!");
         SpringApplication.run(DbFinalApplication.class, args);
     }
 
