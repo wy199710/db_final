16b012b10c2c8450b18871b5818733fab18f3577
dxue9 <dxue9@wisc.edu>
2022-05-02 00:49:26 -0400

    m

--- src/main/java/com/example/db_final/model/User.java
+++ src/main/java/com/example/db_final/model/User.java
@@ -1,10 +1,20 @@
 package com.example.db_final.model;
-
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.apache.ibatis.type.Alias;
 import lombok.Data;
 
 @Data
 public class User {
-    private int id;
+    private Integer uid;
     private String username;
     private String password;
+    private String email;
+    private String profile;
+    private String level;
+    private String city;
+    private String state;
+    private String country;
 }
