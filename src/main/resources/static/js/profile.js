b4f4bd2a8656f943b7bbc252f52533bf78663e75
dxue9 <dxue9@wisc.edu>
2022-05-05 01:45:55 -0400

    css

---
+++ src/main/resources/static/js/profile.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+    $.getJSON("http://localhost:5000/getuserinfo",function(msg){
+
+        if(msg.status == 404)
+            alert("Please login!");
+        else
+        {   console.log(msg.user.username);
+            $("#username").html("Hello, " + msg.user.username+"!");
+            $("#email").html("My email: " + msg.user.email);
+            $("#address").html("My address: " + msg.user.city + ", " + msg.user.state + ", " + msg.user.country);
+            $("#point").html("My current points: " + msg.user.point);
+            $("#level").html("My current level: " + msg.user.level);
+        }
+    });
+})
